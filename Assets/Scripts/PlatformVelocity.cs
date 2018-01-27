bc11c0c74e3cf882697095865e08f1675448dde6
AIELocal <AIELocal@SEA-167637.ad.aie.edu>
2018-01-26 22:19:40 -0800

    First commit

---
+++ Assets/Scripts/PlatformVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+
+    public float waveDelayTime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        WaveDelay(waveDelayTime, collider);
+    }
+
+    private IEnumerator WaveDelay(float delayTime, Collider collider)
+    {
+
+        yield return new WaitForSeconds(delayTime);
+    }
+}
