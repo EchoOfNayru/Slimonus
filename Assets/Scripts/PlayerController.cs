36038a7aa04b30d2ad0c3c59b6f0b2c5852883c7
AIELocal <AIELocal@SEA-167637.ad.aie.edu>
2018-01-26 23:56:23 -0800

    Velocity/bounce added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        if (Input.GetAxis("Horizontal") == 1)
+        {
+            Debug.Log("this way");
+        }
+	}
+}
